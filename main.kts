985b076ea84260c9c4ffdcaa4572459271d611a9
Elisa <elisat15@uw.edu>
2019-04-14 22:26:12 -0700

    pass all test

--- main.kts
+++ main.kts
@@ -3,10 +3,29 @@ println("UW Complex Kotlin homework")
 // write a lambda using map and fold to solve "FIZZBUZZ" for the first fifteen numbers (0..15)
 // use map to return a list with "", "FIZZ" or "BUZZ" as necessary
 // use fold to compress the array of strings down into a single string
-// the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
+// the final string should look like 
+// expected: FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
+// result:   FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
 //
-val mapFoldResults = ""
+var mapFoldResults = ""
+var numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 
+var phrases = numbers.map({ 
+    num: Int ->
+    if (num % 3 == 0 && num % 5 == 0)
+        "FIZZBUZZ"
+    else if (num % 3 == 0)
+        "FIZZ"
+    else if (num % 5 == 0)
+        "BUZZ"
+    else 
+        ""
+})
+
+mapFoldResults = phrases.fold("", {
+    acc: String, phrase: String ->
+    acc + phrase
+})
 
 // This is a utility function for your use as you choose, and as an
 // example of an extension method
@@ -20,21 +39,43 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-val r1 = "" // call process() with message "FOO" and a block that returns "BAR"
+val r1 = process("FOO", {String -> "BAR"}) // call process() with message "FOO" and a block that returns "BAR"
 
 val r2_message = "wooga"
-val r2 = "" // call process() with message "FOO" and a block that upper-cases 
+val r2 = process("FOO") {
+    String ->
+    var returnMessage = ""
+    for (i in 1..3)
+        returnMessage = returnMessage + r2_message.toUpperCase()
+    returnMessage
+} // call process() with message "FOO" and a block that upper-cases 
             // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher { 
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString() = "Deep thoughts...."
+
+    },
+
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString() = "Allow me to suggest an idea..."
+    };
+
+    abstract fun signal(): Philosopher
+}
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
 // primary constructor should take a String argument ("prompt")
 // when called, the Command object should return a String containing the prompt and then the message
 class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
 }
 
 
